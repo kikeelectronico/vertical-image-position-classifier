fe7003f7d788e6fd52c42d048c053a3e705905c2
enrique.gomez <enrique.gomez@bq.com>
2023-02-14 22:12:47 +0100

    draw the rectangles always

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ files_names = os.listdir("./images")
 
 for file_name in files_names:
     show_image = True
-    index = -1
+    index = 0
     while show_image:
         # Read the image
         image = cv2.imread("images/" + file_name)
@@ -16,13 +16,12 @@ for file_name in files_names:
             y = count * int(height/10)
             cv2.line(image, (0, y), (width, y), (255, 0, 0), 1, 1)
             cv2.putText(image, str(count), (10, y + 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255))
-        # Draw rectangles
-        if not index == -1:
-            start_point = (0, index * int(height/10))
-            end_point = (width, ((index + 5) * int(height/10)) + int((height/10) * 0.6))
-            cv2.rectangle(image, start_point, end_point, (0, 255, 0), 2)
-            end_point = (width, (index + 2) * int(height/10))
-            cv2.rectangle(image, start_point, end_point, (255, 0, 0), 2)
+        # Draw view port rectangles
+        start_point = (0, index * int(height/10))
+        end_point = (width, ((index + 5) * int(height/10)) + int((height/10) * 0.6))
+        cv2.rectangle(image, start_point, end_point, (0, 255, 0), 2)
+        end_point = (width, (index + 2) * int(height/10))
+        cv2.rectangle(image, start_point, end_point, (255, 0, 0), 2)
         # Show the image
         cv2.imshow('frame', image)
         # Wait for user input
